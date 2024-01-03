975218cd588a7d56ae42f261254e50084104555b
Richard Shaaya <rshaaya@rtechservices.co>
2024-01-02 23:01:53 -0500

    Fixed asset bug

--- js/game.js
+++ js/game.js
@@ -34,12 +34,12 @@ var vX = 0,
 
 //load our images
 resources.load([
-  ASSETS_ROOT + '/sprites/player.png',
-  ASSETS_ROOT + '/sprites/enemy.png',
-  ASSETS_ROOT + '/sprites/tiles.png',
-  ASSETS_ROOT + '/sprites/playerl.png',
-  ASSETS_ROOT + '/sprites/items.png',
-  ASSETS_ROOT + '/sprites/enemyr.png',
+  'sprites/player.png',
+  'sprites/enemy.png',
+  'sprites/tiles.png',
+  'sprites/playerl.png',
+  'sprites/items.png',
+  'sprites/enemyr.png',
 ]);
 
 resources.onReady(init);
@@ -51,24 +51,24 @@ var music;
 var lastTime;
 function init() {
   music = {
-    overworld: new Audio(ASSETS_ROOT + '/sounds/aboveground_bgm.ogg'),
-    underground: new Audio(ASSETS_ROOT + '/sounds/underground_bgm.ogg'),
-    clear: new Audio(ASSETS_ROOT + '/sounds/stage_clear.wav'),
-    death: new Audio(ASSETS_ROOT + '/sounds/mariodie.wav')
+    overworld: new Audio('sounds/aboveground_bgm.ogg'),
+    underground: new Audio('sounds/underground_bgm.ogg'),
+    clear: new Audio('sounds/stage_clear.wav'),
+    death: new Audio('sounds/mariodie.wav')
   };
   sounds = {
-    smallJump: new Audio(ASSETS_ROOT + '/sounds/jump-small.wav'),
-    bigJump: new Audio(ASSETS_ROOT + '/sounds/jump-super.wav'),
-    breakBlock: new Audio(ASSETS_ROOT + '/sounds/breakblock.wav'),
-    bump: new Audio(ASSETS_ROOT + '/sounds/bump.wav'),
-    coin: new Audio(ASSETS_ROOT + '/sounds/coin.wav'),
-    fireball: new Audio(ASSETS_ROOT + '/sounds/fireball.wav'),
-    flagpole: new Audio(ASSETS_ROOT + '/sounds/flagpole.wav'),
-    kick: new Audio(ASSETS_ROOT + '/sounds/kick.wav'),
-    pipe: new Audio(ASSETS_ROOT + '/sounds/pipe.wav'),
-    itemAppear: new Audio(ASSETS_ROOT + '/sounds/itemAppear.wav'),
-    powerup: new Audio(ASSETS_ROOT + '/sounds/powerup.wav'),
-    stomp: new Audio(ASSETS_ROOT + '/sounds/stomp.wav')
+    smallJump: new Audio('sounds/jump-small.wav'),
+    bigJump: new Audio('sounds/jump-super.wav'),
+    breakBlock: new Audio('sounds/breakblock.wav'),
+    bump: new Audio('sounds/bump.wav'),
+    coin: new Audio('sounds/coin.wav'),
+    fireball: new Audio('sounds/fireball.wav'),
+    flagpole: new Audio('sounds/flagpole.wav'),
+    kick: new Audio('sounds/kick.wav'),
+    pipe: new Audio('sounds/pipe.wav'),
+    itemAppear: new Audio('sounds/itemAppear.wav'),
+    powerup: new Audio('sounds/powerup.wav'),
+    stomp: new Audio('sounds/stomp.wav')
   };
   Mario.oneone();
   lastTime = Date.now();
--- js/levels/11.js
+++ js/levels/11.js
@@ -10,69 +10,69 @@ var oneone = Mario.oneone = function() {
     scrolling: true,
     invincibility: [144, 192, 240],
     exit: 204,
-    floorSprite:  new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0,0],[16,16],0),
-    cloudSprite:  new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0,320],[48,32],0),
-    wallSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0, 16],[16,16],0),
+    floorSprite:  new Mario.Sprite('sprites/tiles.png', [0,0],[16,16],0),
+    cloudSprite:  new Mario.Sprite('sprites/tiles.png', [0,320],[48,32],0),
+    wallSprite: new Mario.Sprite('sprites/tiles.png', [0, 16],[16,16],0),
 
-    houseRoofSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [176, 00],[16,16],0),
-    houseRoofTopSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [176, 16],[16,16],0),
-    houseSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [208, 00],[16,16],0),
-    houseLeftWindowSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [192, 00],[16,16],0),
-    houseRightWindowSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [224, 00],[16,16],0),
-    houseDoorTopSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [192, 16],[16,16],0),
-    houseDoorBottomSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [208, 16],[16,16],0),
+    houseRoofSprite: new Mario.Sprite('sprites/tiles.png', [176, 00],[16,16],0),
+    houseRoofTopSprite: new Mario.Sprite('sprites/tiles.png', [176, 16],[16,16],0),
+    houseSprite: new Mario.Sprite('sprites/tiles.png', [208, 00],[16,16],0),
+    houseLeftWindowSprite: new Mario.Sprite('sprites/tiles.png', [192, 00],[16,16],0),
+    houseRightWindowSprite: new Mario.Sprite('sprites/tiles.png', [224, 00],[16,16],0),
+    houseDoorTopSprite: new Mario.Sprite('sprites/tiles.png', [192, 16],[16,16],0),
+    houseDoorBottomSprite: new Mario.Sprite('sprites/tiles.png', [208, 16],[16,16],0),
 
 
-    brickSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [16, 0], [16,16], 0),
-    brickBounceSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png',[32,0],[16,16],0),
+    brickSprite: new Mario.Sprite('sprites/tiles.png', [16, 0], [16,16], 0),
+    brickBounceSprite: new Mario.Sprite('sprites/tiles.png',[32,0],[16,16],0),
     rubbleSprite: function () {
-      return new Mario.Sprite(ASSETS_ROOT + '/sprites/items.png', [64,0], [8,8], 3, [0,1])
+      return new Mario.Sprite('sprites/items.png', [64,0], [8,8], 3, [0,1])
     },
-    ublockSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [48, 0], [16,16],0),
-    superShroomSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/items.png', [0,0], [16,16], 0),
-    fireFlowerSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/items.png', [0,32], [16,16], 20, [0,1,2,3]),
-    starSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/items.png', [0,48], [16,16], 20, [0,1,2,3]),
-    pipeLEndSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0, 128], [16,16], 0),
-    pipeREndSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [16, 128], [16,16], 0),
-    pipeLMidSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0, 144], [16,16], 0),
-    pipeRMidSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [16, 144], [16,16], 0),
+    ublockSprite: new Mario.Sprite('sprites/tiles.png', [48, 0], [16,16],0),
+    superShroomSprite: new Mario.Sprite('sprites/items.png', [0,0], [16,16], 0),
+    fireFlowerSprite: new Mario.Sprite('sprites/items.png', [0,32], [16,16], 20, [0,1,2,3]),
+    starSprite: new Mario.Sprite('sprites/items.png', [0,48], [16,16], 20, [0,1,2,3]),
+    pipeLEndSprite: new Mario.Sprite('sprites/tiles.png', [0, 128], [16,16], 0),
+    pipeREndSprite: new Mario.Sprite('sprites/tiles.png', [16, 128], [16,16], 0),
+    pipeLMidSprite: new Mario.Sprite('sprites/tiles.png', [0, 144], [16,16], 0),
+    pipeRMidSprite: new Mario.Sprite('sprites/tiles.png', [16, 144], [16,16], 0),
 
-    pipeUpMid: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0, 144], [32,16], 0),
-    pipeSideMid: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [48, 128], [16,32], 0),
-    pipeLeft: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [32, 128], [16,32], 0),
-    pipeTop: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0, 128], [32,16], 0),
-    qblockSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [384, 0], [16,16], 8, [0,0,0,0,1,2,1]),
+    pipeUpMid: new Mario.Sprite('sprites/tiles.png', [0, 144], [32,16], 0),
+    pipeSideMid: new Mario.Sprite('sprites/tiles.png', [48, 128], [16,32], 0),
+    pipeLeft: new Mario.Sprite('sprites/tiles.png', [32, 128], [16,32], 0),
+    pipeTop: new Mario.Sprite('sprites/tiles.png', [0, 128], [32,16], 0),
+    qblockSprite: new Mario.Sprite('sprites/tiles.png', [384, 0], [16,16], 8, [0,0,0,0,1,2,1]),
     bcoinSprite: function() {
-      return new Mario.Sprite(ASSETS_ROOT + '/sprites/items.png', [0,112],[16,16], 20,[0,1,2,3]);
+      return new Mario.Sprite('sprites/items.png', [0,112],[16,16], 20,[0,1,2,3]);
     },
     cloudSprites:[
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [0,320],[16,32],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [16,320],[16,32],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [32,320],[16,32],0)
+      new Mario.Sprite('sprites/tiles.png', [0,320],[16,32],0),
+      new Mario.Sprite('sprites/tiles.png', [16,320],[16,32],0),
+      new Mario.Sprite('sprites/tiles.png', [32,320],[16,32],0)
     ],
     hillSprites: [
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [128,128],[16,16],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [144,128],[16,16],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [160,128],[16,16],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [128,144],[16,16],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [144,144],[16,16],0),
-      new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [160,144],[16,16],0)
+      new Mario.Sprite('sprites/tiles.png', [128,128],[16,16],0),
+      new Mario.Sprite('sprites/tiles.png', [144,128],[16,16],0),
+      new Mario.Sprite('sprites/tiles.png', [160,128],[16,16],0),
+      new Mario.Sprite('sprites/tiles.png', [128,144],[16,16],0),
+      new Mario.Sprite('sprites/tiles.png', [144,144],[16,16],0),
+      new Mario.Sprite('sprites/tiles.png', [160,144],[16,16],0)
     ],
-    bushSprite: new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [176, 144], [48, 16], 0),
+    bushSprite: new Mario.Sprite('sprites/tiles.png', [176, 144], [48, 16], 0),
     bushSprites: [
-     new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [176,144], [16,16],0),
-     new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [192,144], [16,16],0),
-     new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [208,144], [16,16],0)],
+     new Mario.Sprite('sprites/tiles.png', [176,144], [16,16],0),
+     new Mario.Sprite('sprites/tiles.png', [192,144], [16,16],0),
+     new Mario.Sprite('sprites/tiles.png', [208,144], [16,16],0)],
    goombaSprite: function() {
-     return new Mario.Sprite(ASSETS_ROOT + '/sprites/enemy.png', [0, 16], [16,16], 3, [0,1]);
+     return new Mario.Sprite('sprites/enemy.png', [0, 16], [16,16], 3, [0,1]);
    },
    koopaSprite: function() {
-     return new Mario.Sprite(ASSETS_ROOT + '/sprites/enemy.png', [96,0], [16,32], 2, [0,1]);
+     return new Mario.Sprite('sprites/enemy.png', [96,0], [16,32], 2, [0,1]);
    },
    flagPoleSprites: [
-     new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [256, 128], [16,16], 0),
-     new Mario.Sprite(ASSETS_ROOT + '/sprites/tiles.png', [256, 144], [16,16], 0),
-     new Mario.Sprite(ASSETS_ROOT + '/sprites/items.png', [128, 32], [16,16], 0)
+     new Mario.Sprite('sprites/tiles.png', [256, 128], [16,16], 0),
+     new Mario.Sprite('sprites/tiles.png', [256, 144], [16,16], 0),
+     new Mario.Sprite('sprites/items.png', [128, 32], [16,16], 0)
    ]
  });
   ground = [[0,69],[71,86],[89,153],[155,212]];
